e8141883e84d7704bcc7be708cc5af6414b2a703
AnasJMirza <ajmhimself@gmail.com>
2022-08-09 20:00:00 +0500

    works on menu items

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import Menu from './components/Menu';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Menu />
+      <Navbar />
+    </Container>
   );
 }
 
 export default App;
+
+
+const Container = styled.div`
+  display: flex;
+  
+`
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Navbar = () => {
+    return (
+        <Container>
+            Navbar
+        </Container>
+    );
+};
+
+export default Navbar;
+
+
+const Container = styled.div`
+    flex: 11;
+`
