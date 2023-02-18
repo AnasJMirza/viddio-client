85b3b9aa067d08226eabdeef22a238177edce84b
AnasJMirza <ajmhimself@gmail.com>
2023-02-19 02:28:39 +0500

    deploy

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://youtube-server-jmq7.onrender.com/api",
     withCredentials: true,
     headers: {
         "Content-type": "application/json",
