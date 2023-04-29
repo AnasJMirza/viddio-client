5e7724b576798210adb5748bcfde019f2b5e53c1
AnasJMirza <ajmhimself@gmail.com>
2023-04-29 23:52:39 +0500

    redeploy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import Card from '../components/Card';
-import axios from '../axios.js'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Card from "../components/Card";
+import axios from "../axios.js";
+import { Puff } from "react-loader-spinner";
 
+const Home = ({ type }) => {
+  const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-const Home = ({type}) => {
+  useEffect(() => {
+    setLoading(true);
+    try {
+      const getVideos = async () => {
+        const res = await axios.get(`/video/${type}`);
+        setVideos(res.data);
+      };
+      getVideos();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }, [type]);
 
-    const [videos, setVideos] = useState([]);
-
-    useEffect(()=>{
-        const getVideos = async () => {
-            const res = await axios.get(`/video/${type}`);
-            setVideos(res.data);
-        }
-        getVideos();
-    }, [type])
-
-    return (
-        <Container>
-            {
-                videos.map((video)=>{
-                    return <Card key={video._id} video={video}/>
-                })
-            }
-        </Container>
-    );
+  return (
+    <Container>
+      {loading ? (
+        // <Puff
+        //   height="80"
+        //   width="80"
+        //   radius={1}
+        //   color="#4fa94d"
+        //   ariaLabel="puff-loading"
+        //   wrapperStyle={{}}
+        //   wrapperClass=""
+        //   visible={true}
+        // /> 
+        "loading....."
+      ) : (
+        videos.map((video) => {
+          return <Card key={video._id} video={video} />;
+        })
+      )}
+    </Container>
+  );
 };
 
 export default Home;
 
-
 const Container = styled.div`
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    margin: 22px 56px;
-`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin: 22px 56px;
+`;
